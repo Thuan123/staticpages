f40a1fe936fa7439e6678fcd922c9f9a48696f95
thuan123 <thuanbk56@gmail.com>
2015-09-26 17:20:06 +0000

    fixhelp

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
+  test "should get help" do
+    get :help
     assert_response :success
   end
 
